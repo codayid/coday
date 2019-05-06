f0f8be2ff87a08e432cea0cb5bc36eb4ef38f8e1
Muhammad Hidayat <49159167+codayid@users.noreply.github.com>
2019-05-07 04:05:23 +0700

    Create codayNEW.sh

---
+++ codayNEW.sh
@@ -0,0 +1,347 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#codayid
+
+#ExCellent : Aryanrtm - Arif - Adji - Ilham - Syaid - Aryaax - Rizky - Reza - Andri - AnonCyberTeam - ACTangerang
+#             Security Darknet - Teh Squad Cyber - Bali BlackHat - CHAOS PHANTEON - InCrustWeRush 
+
+#TOOLS
+
+clear 
+
+echo -e "\e[1;31m
+       ____ ___  ____    _   __   ___  _ _____
+      / ___/ _ \|  _ \  / \  \ \ / / || |_   _| __
+  ___| |  | | | | | | |/ _ \  \ V /| || |_| |__/ /
+ / __| |__| |_| | |_| / ___ \  | | |__   _| |___/
+/_/   \____\___/|____/_/   \_\ |_|    |_| |_|"
+
+sleep 2
+
+echo -e "\033[92m        *****************************************"
+echo -e "        *                                       *"
+echo -e "*********************************************   *"
+echo -e "* Author ; CodayID                          *   *"
+echo -e "* TEAM   ; AnonCyberTeam x TEH Squad Cyber  *   *"
+echo -e "* Greetz ; Aryanrtm - Arif - Adji - Ilham   *   *"
+echo -e "* Suport ; P#puD {Putri Amelia Arsyad}      *   *"
+echo -e "* MyLov3 ; Dew! {Retno Dewi}                *   *"
+echo -e "* Contact; @coday.id / +62 888-0928-0542    *   *"
+echo -e "* Tools  ; Installer Tools 50% :3           *   *"
+echo -e "*********************************************   *"
+sleep 2
+echo -e "* 1>  Metaspoit                             *   *"
+echo -e "* 2>  SQLMap                                *   *"
+echo -e "* 3>  YahooCloning                          *   *"
+echo -e "* 4>  MultiBruteForce [FB]                  *   *"
+echo -e "* 5>  Instafuck3r                           *   *"
+echo -e "* 6>  NiKK_G3N                              *   *"
+echo -e "* 7>  SPAM Grab                             *   *"
+echo -e "* 8>  BruteForce [FB_Target]                *   *"
+echo -e "* 9>  SQLMate                               *   *"
+echo -e "* 10> RED_HAWK                              *   *"
+echo -e "* 11> Shell Backdoor [IDX]                  *   *"
+echo -e "* 12> BruteForce [WordPress_LOG]            *   *"
+echo -e "* 13> SPAM Bukalapak                        *   *"
+echo -e "* 14> CmatriX                               * ***"
+echo -e "* 15> Exit                                  *"
+sleep 2
+echo -e "*********************************************"
+echo -e "*         Copyright # CodayID 2K19          *"
+echo -e "*********************************************"
+echo -e ""
+read -p "\e[1;31m(=CodayID=)=> " yat;
+
+clear
+
+if [ $yat = 1 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/rapid7/metasploit-framework/
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 2 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/sqlmapproject/sqlmap
+
+    apt install python2 -y 
+
+    cd sqlmap
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 3 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/wahyuandhika/YahooCloning
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 4 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/pirmansx/mbf 
+
+    apt install python2 -y 
+
+    cd mbf
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 5 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/aryanrtm/instafuck3r
+
+    apt install bash -y 
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 6 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    nano nikk.txt
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 7 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+    
+    cd ..
+    
+    apt install php -y
+
+    git clone https://github.com/4L13199/LITESPAM/Tul
+
+    php 3.php
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 8 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    curl -S https://pastebin.com/raw/yUpg8zsv --output fb.py
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 9 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/s0md3v/sqlmate
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 10 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 11 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    nano shell.txt
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 12 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    cd ..
+    
+    apt install php -y
+
+    git clone https://github.com/4L13199/LITESPAM/Tul
+
+    php 1.php
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 13 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+     git clone https://github.com/Moham3dRiahi/XBruteForcer.git
+
+    echo -e " REDDY - DONE"
+fi
+
+
+if [ $yat = 14 ]
+
+then
+	clear
+
+	echo -e " WAIT - INSTALLING "
+
+sleep 1
+
+    clear
+
+    apt install chmatrix
+
+    cmatrix 
+
+    echo -e " REDDY - DONE"
+fi
+
+if [ $yat = 15 ]
+
+then
+
+clear
+ 
+
+echo -e "
+#############################
+| |  __    __     ___     _
+| | / /   /  \   |   \   | |
+| |/ /   / || \  | |\ \  | | \ \ 
+|   <   <  ||  > | | \ \ | |  \ \  
+| |\ \   \ || /  | |  \ \| |   \ \ 
+|_| \_\   \__/   |_|   \___|    \ \ 
+#############################    \ \ "
+sleep 3
+echo -e " _______   ___    _       _      / /
+|__   __| / _ \  | |     | |    / /
+   | |   / | | \ | |     | | \ /  \ /
+   | |   \ |_| / | |___  |_|  \ || /
+   |_|    \___/  |_____|  _    \__/
+                         |_|
+#############################"
+
+
+
+exit
+
+fi 
